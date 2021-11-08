9c31e7f1d500d9fdfd290a19f1ec19e7a833c2ca
Jordan Passant <jordan.passant@gmail.com>
2021-11-08 13:59:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ const productRouter = require('./routes/product');
 app.use(express.json());
 app.use('/product', productRouter);
 
+// NEW - Add CORS headers - see https://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('<h1>Node.js CRUD API</h1> <h4>Message: Success</h4><p>Version: 1.0</p>');
 })
