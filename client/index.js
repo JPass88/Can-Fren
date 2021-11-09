24c7bde826ac9f86afaf6881235805dd2124ab95
Jordan Passant <jordan.passant@gmail.com>
2021-11-08 22:18:29 -0500

    omg will this work

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
  document.addEventListener('DOMContentLoaded', function () {
-         fetch('http://localhost:5000/getAll')
+         fetch('http://tutorialnodejscrudapipipe-env.eba-ac7mk4vc.us-east-2.elasticbeanstalk.com/getAll')
          .then(response => response.json())
          .then(data => loadHTMLTable(data['data']));    
 });
